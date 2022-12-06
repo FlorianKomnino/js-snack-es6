4835aa500ad3d41f7fe20cfdaaafbaa74b896d11
FlorianKomnino <florian.komnino@outlook.it>
2022-12-06 11:29:21 +0100

    created animals array, added .filter to get only mammals

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -1,2 +1,36 @@
 console.log('Hello Universe!');
 
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+// Es:
+
+// [
+//   { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+//   { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+//   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+// ]
+
+// Crea un nuovo array con la lista dei mammiferi.
+// Bonus 1 : trasforma ogni valore di ogni animale in tutto maiuscolo tranne la prima lettera, tipo // leone => lEONE, canidi => cANIDI
+
+
+let animali = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: 'gatto', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'lupo', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'fagiano', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: 'lince', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'un altro cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallo', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+
+const soloMammiferi = animali.filter((elemento) => {
+    if (elemento.classe == 'mammiferi'){
+        return true;
+    }
+    return false;
+})
+
+console.log(soloMammiferi)
