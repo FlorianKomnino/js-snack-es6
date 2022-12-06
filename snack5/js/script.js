597ff7bee32e47119630744d075689ff3c241f63
FlorianKomnino <florian.komnino@outlook.it>
2022-12-06 11:57:32 +0100

    added people array

--- snack5/js/script.js
+++ snack5/js/script.js
@@ -1,2 +1,65 @@
 console.log('Hello Universe!');
 
+/***
+ * 
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un'età, creiamone almeno una decina.
+
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+ */
+
+let people = [
+    {
+        name: 'Gianpinuccia',
+        surname: 'Patrupilli',
+        age: 91
+    },
+
+    {
+        name: 'Pierino',
+        surname: 'Patrupilli',
+        age: 91
+    },
+    {
+        name: 'Salvatauro',
+        surname: 'Patrupilli',
+        age: 91
+    },
+    {
+        name: 'GiadaScript',
+        surname: 'Patrupilli',
+        age: 91
+    },
+    {
+        name: 'Donatonni',
+        surname: 'Patrupilli',
+        age: 91
+    },
+    {
+        name: 'Salvio',
+        surname: 'Patrupilli',
+        age: 91
+    },
+    {
+        name: 'Checca',
+        surname: 'Patrupilli',
+        age: 91
+    },
+    {
+        name: 'Tantania',
+        surname: 'Patrupilli',
+        age: 91
+    },
+    {
+        name: 'Bimbumbanksi',
+        surname: 'Patrupilli',
+        age: 91
+    },
+    {
+        name: 'Uga',
+        surname: 'Patrupilli',
+        age: 91
+    }
+];
+
